33793beacb3f08a96e037036bc074c937c0048bb
RyanAkmalRamadhan <134803527+RyanAkmalRamadhan@users.noreply.github.com>
2023-07-11 14:03:07 +0800

    Merapikan kembali

--- src/TokoBukuAksi.java
+++ src/TokoBukuAksi.java
@@ -7,6 +7,11 @@ public class TokoBukuAksi {
         A.harga = 50000;
         A.penulis = "Ahmad";
 
+        TokoBuku B = new TokoBuku();
+        B.nama = "Buku Java";
+        B.harga = 65000;
+        B.penulis = "Budi";
+
         System.out.println("===============TOKO BUKU MERDEKA===============");
         System.out.println("Buku yang sedang promo mendapatkan potongan 10K");
         System.out.println("===============================================");
@@ -16,12 +21,7 @@ public class TokoBukuAksi {
 
         A.mendapatDiskon();
         System.out.println("  Harga Buku Setelah Mendapat Diskon  : "+A.harga);
-
-
-        TokoBuku B = new TokoBuku();
-        B.nama = "Buku Java";
-        B.harga = 65000;
-        B.penulis = "Budi";
+        System.out.println("===============================================");
 
         System.out.println("2.Nama Buku    : "+B.nama);
         System.out.println("  Harga Buku   : Rp."+B.harga);
